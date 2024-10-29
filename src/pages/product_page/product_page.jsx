45d2dc79e97f68e271eefc305c26d7c8ed404539
ibrohim74 <xibroxim@gmail.com>
2024-10-29 18:53:14 +0500

    update

--- src/pages/product_page/product_page.jsx
+++ src/pages/product_page/product_page.jsx
@@ -10,7 +10,6 @@ const ProductPage = () => {
         'https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_640.jpg',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDR8H0rgV-zmSodkT_erGjzA_VhfWE22Pg7Q&s',
         'https://www.recordnet.com/gcdn/presto/2021/03/22/NRCD/9d9dd9e4-e84a-402e-ba8f-daa659e6e6c5-PhotoWord_003.JPG?width=660&height=425&fit=crop&format=pjpg&auto=webp'
-        // добавьте пути к вашим изображениям
     ];
 
     return (
@@ -28,7 +27,7 @@ const ProductPage = () => {
                 </div>
                 <CoruselBottom />
             </div>
-            {/* left bar  */}
+            
             <div>
                 <Leftbar />
             </div>
